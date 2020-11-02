6ac3df6a82b733ae3c65ec76364bc5e2542c2943
Mujahidul-Islam <40883770+Mujahidul-Islam@users.noreply.github.com>
2020-11-01 18:00:31 -0600

    "Exception-handling Practice"

---
+++ src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        try  {
+            Scanner scan = new Scanner(System.in);
+           int  x = scan.nextInt();
+           int y = scan.nextInt();
+            System.out.println(x / y);
+
+        }
+        catch (InputMismatchException e){
+            System.out.println("java.util.InputMismatchException");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
